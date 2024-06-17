48373405d5eb47b3ec7fcc3b9b818445a55e6eb9
Maximiliano Moscoso <moscosomaximiliano@gmail.com>
2024-06-17 19:45:56 -0300

    add card component

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,27 @@ import { useState } from 'react'
 
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
+import { CardDefault } from './components/Card.jsx'
 import { useEffect } from 'react'
 import axios from 'axios'
 
-function App() {
-  const [ products, setProducts ] = useState([])
-  const [ loggedIn, setLoggedIn ] = useState(false)
+function App () {
+  const [products, setProducts] = useState([])
+  const [loggedIn, setLoggedIn] = useState(false)
   useEffect(() => {
-    axios('http://localhost:9999/api/products', { withCredentials: true }).then(
-      response => setProducts(response.data)
-    ).catch(
-      error => console.log(error)
-    )
+    axios(`${process.env.REACT_APP_BASE_URL}/api/products`, {
+      withCredentials: true
+    })
+      .then(response => {
+        setProducts(response.data.response.docs)
+      })
+      .catch(error => console.log(error))
   }, [])
   return (
     <>
       <Navbar logged={loggedIn} />
-      {products.map((product) => (
-        <div key={product.id}>{product}</div>
+      {products.map(product => (
+        <CardDefault key={product._id} product={product} />
       ))}
       <Footer />
     </>
